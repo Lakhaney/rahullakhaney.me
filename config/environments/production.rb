5a8be8f6dd34104d6b3ce3faa2bd51035e462ba6
Rahul Lakhaney <rahul.lakhaney@gmail.com>
2015-10-05 00:13:18 +0530

    Fixed typo in the environment/production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  onfig.action_mailer.default_url_options = { host: 'http://www.rahullakhaney.me' }
+  config.action_mailer.default_url_options = { host: 'http://www.rahullakhaney.me' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {

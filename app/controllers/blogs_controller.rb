aae1b5baeb2ed04033b5d1d079d2819f979debdb
Rahul Lakhaney <rahul.lakhaney@gmail.com>
2015-10-04 15:55:50 +0530

    Added Blog segment with CRUD ability

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,50 @@
+class BlogsController < ApplicationController
+	before_action :find_blog, only: [:show, :edit, :update, :destroy]
+
+	def index
+	end
+
+	def new
+		@blog = Blog.new
+	end
+
+	def create
+		@blog = Blog.new blog_params
+
+		if @blog.save
+			redirect_to @blog, notice: "Hell Yeah Rahul! Article Published Successfully"
+		else
+			render 'new', notice: "Oh no! Rahul! I was unable to save your post"
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @blog.update blog_params
+			redirect_to @blog, notice: "Yabadabadoo! Article Updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@blog.destroy
+		redirect_to blogs_path
+	end
+
+	private
+
+	def blog_params
+		params.require(:blog).permit(:title, :content)
+	end
+
+	def find_blog
+		@blog = Blog.find(params[:id])
+	end
+
+end

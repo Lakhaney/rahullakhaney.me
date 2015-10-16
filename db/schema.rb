d59615448887619b0edd85cbd7fbca048326d860
Rahul Lakhaney <rahul.lakhaney@gmail.com>
2015-10-17 01:30:22 +0530

    Added github repo button to projects show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004171902) do
+ActiveRecord::Schema.define(version: 20151016195459) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20151004171902) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "slug"
+    t.string   "github_link"
   end
 
   add_index "projects", ["slug"], name: "index_projects_on_slug", unique: true
